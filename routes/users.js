f8f1e450db2cd5443db37c721952eedf794e6e72
CodeGK <codegeek1991@gmail.com>
2017-09-17 23:36:49 -0500

    testing multer

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
+var upload = multer();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/test', upload.array(), function(req, res, next) {
+  console.log(req.body);
+  res.status(200);
+  res.send('hi');
+});
+
 
 module.exports = router;
