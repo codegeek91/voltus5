c093d83cfd3f4defe5c36925e6998a52e590c2f9
CodeGK <codegeek1991@gmail.com>
2017-09-20 03:30:33 -0500

    adding readfile sync

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var multer  = require('multer');
 var upload = multer();
 const sgMail = require('@sendgrid/mail');
+var fs = require('fs');
+var apikey = fs.readFileSync('sendgridkey.txt', 'utf8');
+console.log(apikey);
 sgMail.setApiKey('api key here');
 
 var Problem = require("../models/problem");
